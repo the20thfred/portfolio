dfcb26180c99e1fc6ea2bb81c61eb170d9de91c8
Fred <shalfredjamez@yahoo.com>
2022-11-05 02:49:08 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="under construction!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+           <code></code>
         </p>
       </main>
 
