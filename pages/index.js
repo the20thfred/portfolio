c1ff56139d753016e3608ce610bdd9e79611b910
Fred <shalfredjamez@yahoo.com>
2022-11-06 02:43:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title=" Fred's Portfolio is Under construction!" />
+        <Header title=" Fred's Portfolio is Under construction!  (o_.)" />
         <p className="description">
            <code></code>
         </p>
